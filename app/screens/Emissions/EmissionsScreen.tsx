2e683317d8630102ee63509e75e649aee45a71e2
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2023-01-22 15:51:43 +0100

    Fetch prices from Binance

--- app/screens/Emissions/EmissionsScreen.tsx
+++ app/screens/Emissions/EmissionsScreen.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable  @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-import React from "react";
-import { SectionList, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SectionList, View, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { filter } from "ramda";
 
@@ -24,29 +24,50 @@ const EmissionsScreen: React.FC<Props> = ({ emissions = [], recurringEmissions }
   const renderListFooter = () => <View style={styles.separator} />;
   const renderSectionHeader = (date, title) => <SectionHeader title={title} date={date} />;
 
+  const [ethPrice, setEthPrice] = useState("0");
+  const [btcPrice, setBtcPrice] = useState("0");
+
+  useEffect(() => {
+    async function fetchPrices() {
+      setEthPrice(
+        (+(await (await fetch("https://api.binance.com/api/v3/avgPrice?symbol=ETHBUSD")).json())
+          .price).toFixed(2)
+      );
+      setBtcPrice(
+        (+(await (await fetch("https://api.binance.com/api/v3/avgPrice?symbol=BTCBUSD")).json())
+          .price).toFixed(2)
+      );
+    }
+    fetchPrices();
+  }, []);
+
   return (
-    <SectionList<EmissionListItemProps>
-      style={styles.container}
-      sections={sections}
-      stickySectionHeadersEnabled
-      ListFooterComponent={renderListFooter}
-      renderSectionHeader={({ section: { date, title } }) => renderSectionHeader(date, title)}
-      keyExtractor={({ id }) => id}
-      renderItem={({
-        item: { id, isMitigated, name, title, co2value, iconName, emissionModelType, times },
-      }) => (
-        <EmissionListItem
-          id={id}
-          isMitigated={isMitigated}
-          name={name}
-          onPress={() => navigator.openEmissionItem({ id, isRecurringEmission: !!times })}
-          title={title}
-          co2value={co2value}
-          iconName={iconName}
-          emissionModelType={emissionModelType}
-        />
-      )}
-    />
+    <>
+      <SectionList<EmissionListItemProps>
+        style={styles.container}
+        sections={sections}
+        stickySectionHeadersEnabled
+        ListFooterComponent={renderListFooter}
+        renderSectionHeader={({ section: { date, title } }) => renderSectionHeader(date, title)}
+        keyExtractor={({ id }) => id}
+        renderItem={({
+          item: { id, isMitigated, name, title, co2value, iconName, emissionModelType, times },
+        }) => (
+          <EmissionListItem
+            id={id}
+            isMitigated={isMitigated}
+            name={name}
+            onPress={() => navigator.openEmissionItem({ id, isRecurringEmission: !!times })}
+            title={title}
+            co2value={co2value}
+            iconName={iconName}
+            emissionModelType={emissionModelType}
+          />
+        )}
+      />
+      <Text>1 ETH = {ethPrice} $</Text>
+      <Text>1 BTC = {btcPrice} $</Text>
+    </>
   );
 };
 
